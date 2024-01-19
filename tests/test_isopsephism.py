273106a22c8e264f9b380dbe5ac963007cdbe2cd
uggerus <uggerus@protonmail.com>
2024-01-19 17:11:45 -0600

    Fixed problem in test file

--- tests/test_isopsephism.py
+++ tests/test_isopsephism.py
@@ -1,5 +1,5 @@
 import pytest
-import isopsephism import isopsephy
+from isopsephism import isopsephy
 
 def test_from_greek_to_num():
     assert isopsephism.isopsephy.convert_word_to_num("ΚΟΥΡΑΙ") == 601
