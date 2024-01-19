1eaa15b8d3ab74f0f0b5a2a0ae7d2eb19b303330
uggerus <uggerus@protonmail.com>
2024-01-19 15:36:41 -0600

    Changed tests to test isopsephism.py class change

--- tests/test_isopsephism.py
+++ tests/test_isopsephism.py
@@ -2,4 +2,4 @@ import pytest
 import isopsephism
 
 def test_from_greek_to_num():
-    assert greek_isopsephy("ΚΟΥΡΑΙ") == 601
+    assert isopsephism.greek_isopsephy.convert_word_to_num("ΚΟΥΡΑΙ") == 601
